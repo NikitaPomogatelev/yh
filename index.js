216030bf5df4584c6240e54cdc9a419c5f08453b
Nikita Pomogatelev <nikitoshelper@mail.ru>
2025-07-01 14:16:46 +0500

    update refactor

--- index.js
+++ index.js
@@ -1,15 +1,8 @@
-const usedIds = new Set();
+let lastId = 0;
 
 // Генерация id
 function generateUniqueId() {
-  let id;
-
-  do {
-    id = Math.floor(Math.random() * 101);
-  } while (usedIds.has(id));
-
-  usedIds.add(id);
-  return id;
+  return ++lastId;
 }
 
 // Задачи
@@ -17,7 +10,7 @@ let tasks = [];
 const completedTasks = [];
 let completedTaskCount = 0;
 
-function setTask(title, description) {
+function addTask(title, description) {
   const newTask = {
     id: generateUniqueId(),
     title,
@@ -41,20 +34,22 @@ function showTasks() {
   for (let task of tasks) {
     console.log(task);
   }
+}
 
-  // tasks.forEach((task) => {
-  //   console.log(task);
-  // });
+function findTaskToIdx(idTask) {
+  const currentTaskIdx = tasks.findIndex((t) => t.id === idTask);
+  if (currentTaskIdx === -1) {
+    console.log("Задача не найдена");
+    return null;
+  }
+  return currentTaskIdx;
 }
 
 function completeTask(idTask) {
-  const index = tasks.findIndex((t) => t.id === idTask);
-  if (index === -1) {
-    console.log("Задача не найдена");
-    return;
-  }
+  const currentTaskIdx = findTaskToIdx(idTask);
+  if (currentTaskIdx === null) return;
 
-  const currentTask = tasks[index];
+  const currentTask = tasks[currentTaskIdx];
   console.log(currentTask);
 
   currentTask.isCompleted = true;
@@ -63,7 +58,7 @@ function completeTask(idTask) {
   completedTasks.push(currentTask);
   completedTaskCount++;
 
-  tasks.splice(index, 1);
+  tasks.splice(currentTaskIdx, 1);
 
   const { id, title, description } = currentTask;
 
@@ -71,15 +66,10 @@ function completeTask(idTask) {
 }
 
 function deleteTask(idTask) {
-  const currentTaskIdx = tasks.findIndex((task) => task.id === idTask);
-
-  if (currentTaskIdx === -1) {
-    console.log("Задача не найдена");
-    return;
-  }
+  const currentTaskIdx = findTaskToIdx(idTask);
+  if (currentTaskIdx === null) return;
 
   const currentTask = tasks[currentTaskIdx];
-  console.log(currentTask);
 
   if (!currentTask.isCompleted) {
     const answer = confirm("Таска еще не выполнена, удалить?");
@@ -100,21 +90,16 @@ function clearTasks() {
   console.log("Все задачи очищены");
 }
 
-setTask("Подготовиться к собесам", "JS: поучить теорию, порешать задачки");
-setTask("Погулять", "Пройтись, проветриться, подышать свежим воздухом");
-setTask("Приготовить обед", "Сварить макароны, пожарить мясо");
-setTask("Увидеться с другом", "Позвонить и назначить встречу");
+addTask("Подготовиться к собесам", "JS: поучить теорию, порешать задачки");
+addTask("Погулять", "Пройтись, проветриться, подышать свежим воздухом");
+addTask("Приготовить обед", "Сварить макароны, пожарить мясо");
+addTask("Увидеться с другом", "Позвонить и назначить встречу");
 
 showTasks();
 
-if (tasks.length > 0) {
-  const secondTaskId = tasks[1].id;
-  deleteTask(secondTaskId);
-}
-if (tasks.length > 0) {
-  const secondTaskId = tasks[0].id;
-  completeTask(secondTaskId);
-}
+deleteTask(1);
+
+completeTask(2);
 
 showTasks();
 
